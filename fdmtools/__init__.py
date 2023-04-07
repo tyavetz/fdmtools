51f087ed8bdf9daa0bc071cd1c15121ba4f1dc16
Tomer Yavetz <46767723+tyavetz@users.noreply.github.com>
2023-04-07 11:26:17 -0400

    Update __init__.py

--- fdmtools/__init__.py
+++ fdmtools/__init__.py
@@ -2,5 +2,8 @@
 fdmtools
 """
 
-__author__ = 'tyavetz <tyavetz@gmail.com>'
-__version__ = '1.0'
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    pass  # package is not installed
